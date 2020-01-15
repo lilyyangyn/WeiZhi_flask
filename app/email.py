90c852ae61cd14e0d0565a56761b5b876cf15919
Lily <yyn605@outlook.com>
2020-01-16 03:10:33 +0800

    use another thread to send email

--- app/email.py
+++ app/email.py
@@ -14,8 +14,8 @@ def send_email(to, subject, template, **kwargs):
 	msg = Message(app.config['CDS_MAIL_SUBJECT_PREFIX'] + '-' + subject, sender=app.config['CDS_MAIL_SENDER'], recipients=[to])
 	msg.body = render_template(template + '.txt', **kwargs)
 	msg.html = render_template(template + '.html', **kwargs)
-	mail.send(msg)
+	#mail.send(msg)
 	# generate thread to send email
-	#thr = Thread(target=send_async_email, args=[app, msg])
-	#thr.start()
-	#return thr
+	thr = Thread(target=send_async_email, args=[app, msg])
+	thr.start()
+	return thr
